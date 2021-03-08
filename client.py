fde5d0277aaae1f39779f949afb24adf7270cab3
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2021-03-08 22:12:57 +0300

    reformat code

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ response = requests.get('http://127.0.0.1:5000/history')
 pprint(response.json())
 
 response = requests.post('http://127.0.0.1:5000/send',
-                        json={'username': 'nick', 'text': 'Hello'})
+                         json={'username': 'nick', 'text': 'Hello'})
 pprint(response.json())
 
 response = requests.get('http://127.0.0.1:5000/history')
