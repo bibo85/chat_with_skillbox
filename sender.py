1a1cefd4d3dab7c163e779d26560102e81dfba3c
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2021-03-10 20:36:27 +0300

    Closing access when entering an incorrect password

--- sender.py
+++ sender.py
@@ -13,3 +13,6 @@ while True:
         'http://127.0.0.1:5000/send',
         json={'username': username, 'password': password, 'text': text}
     )
+    if not response.json()['ok']:
+        print('Access denied')
+        break
